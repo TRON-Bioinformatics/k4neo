3a4b154d2147d9c799ccd69ccddeb898626dd85e
Johannes Hausmann <johannes.hausmann@posteo.de>
2026-07-09 14:47:51 +0200

    chore(helper): used rename sample rate function from annotator

--- k4neo/helper/helper.py
+++ k4neo/helper/helper.py
@@ -296,7 +296,7 @@ class Worker:
             query = Queries(database_handle)
             results = annotator.annotate_cts(df_chunk, query)
             sample_hits = annotator.annotate_sequences(results)
-            healthy_sample_rate, tumor_sample_rate, index_sample_rate = annotator.annotate_sample_rate2(results, query)
+            healthy_sample_rate, tumor_sample_rate, index_sample_rate = annotator.annotate_sample_rate(results, query)
 
         return sample_hits, healthy_sample_rate, tumor_sample_rate, index_sample_rate
 
